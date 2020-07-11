e7c787e67a94e2b0cea95c344a3f6584218ef1b3
codaks <chitranshkulshresgtha.cs@geu.ac.in>
2020-07-11 12:41:39 +0530

    Recusrsive Approch For MCM

--- Matrix Chain Multiplication.cpp
+++ Matrix Chain Multiplication.cpp
@@ -1,7 +1,37 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+int matrixChainMultiplication(vector<long long int> vct, long long int i, long long int j)
+{
+    long long int min = 100000000000;
+    if (i >= j)
+    {
+        return 0;
+    }
+
+    for (int k = i; k < j; k++)
+    {
+        int ans = matrixChainMultiplication(vct, i, k) + matrixChainMultiplication(vct, k + 1, j) + vct[i - 1] * vct[k] * vct[j];
+        if (min > ans)
+        {
+            min = ans;
+        }
+    }
+    return min;
+}
+
+int main()
+{
+    long long int n, data;
+    cin >> n;
+    vector<long long int> vct;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> data;
+        vct.push_back(data);
+    }
+
+    cout << matrixChainMultiplication(vct, 1, n - 1);
+
     return 0;
 }
